d78c402723bcd73c5faa9b66ebc4452ee46315bb
Amanda Hayes <hayesmdesign@gmail.com>
2021-10-05 20:12:50 -0700

    refactor and code cleanup

--- mmo-info/src/App.js
+++ mmo-info/src/App.js
@@ -1,44 +1,21 @@
 import { useState, useEffect } from "react";
 import { getAllGames } from "./services/games";
 import "./App.css";
-import { Card, CardContent, Button, Box, Grid } from "@mui/material";
+import { Card, CardContent, Button, Grid } from "@mui/material";
 
 function App() {
   const [mmoData, setMmoData] = useState([]);
-  const [nextUrl, setNextUrl] = useState("");
-  const [prevUrl, setPrevUrl] = useState("");
   const [loading, setLoading] = useState(true);
   const initialURL = "https://www.mmobomb.com/api1/games";
 
   useEffect(() => {
     async function fetchData() {
       let response = await getAllGames(initialURL);
-      console.log(response);
-      setNextUrl(response.next);
-      setPrevUrl(response.previous);
       setMmoData(response);
       setLoading(false);
     }
     fetchData();
   }, []);
-
-  const next = async () => {
-    setLoading(true);
-    let data = await getAllGames(nextUrl);
-    setNextUrl(data.next);
-    setPrevUrl(data.previous);
-    setLoading(false);
-  };
-
-  const prev = async () => {
-    if (!prevUrl) return;
-    setLoading(true);
-    let data = await getAllGames(prevUrl);
-    setNextUrl(data.next);
-    setPrevUrl(data.previous);
-    setMmoData(data);
-    setLoading(false);
-  };
 
   return (
     <div className="App">
@@ -46,34 +23,31 @@ function App() {
         <h1>Loading...</h1>
       ) : (
         <>
-          <Button onClick={prev}>Prev</Button>
-          <Button onClick={next}>Next</Button>
           <h1>MMOs for Every Taste</h1>
           <h3>Looking for a new MMO to try? Look no further!</h3>
           <div>
             {mmoData.map((data) => {
               return (
-                <Grid container sx={{ flexGrow: 1 }} spacing={2}>
-                  <Grid item sx={4}>
-                    <li key={data.id} {...data}>
-                      <Card
-                        sx={{ minWidth: 345, padding: 2 }}
-                        variant="outlined"
+                <li key={data.id} {...data}>
+                  <Card sx={{ minWidth: 345, padding: 2 }} variant="outlined">
+                    <CardContent>
+                      <h3>{data.title}</h3>
+                      {data.short_description}
+                      <br />
+                      <br />
+                      <Button
+                        variant="contained"
+                        size="small"
+                        href={data.game_url}
                       >
-                        <CardContent>
-                          <h3>{data.title}</h3>
-                          {data.short_description}
-                          <br />
-                          <Button variant="contained" href={data.game_url}>
-                            LEARN MORE
-                          </Button>
-                          <br />
-                          {data.platform}
-                        </CardContent>
-                      </Card>
-                    </li>
-                  </Grid>
-                </Grid>
+                        LEARN MORE
+                      </Button>
+                      <br />
+                      <br />
+                      {data.platform}
+                    </CardContent>
+                  </Card>
+                </li>
               );
             })}
           </div>
--- mmo-info/src/services/games.js
+++ mmo-info/src/services/games.js
@@ -7,13 +7,3 @@ export async function getAllGames(url) {
       });
   });
 }
-
-export async function getGames(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        resolve(data);
-      });
-  });
-}
